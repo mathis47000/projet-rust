c7b0bc617e10d8f63fe8b979e74446f0c9351f15
mathis47000 <mathisbescond3@gmail.com>
2025-01-10 09:49:16 +0100

    professor test's

--- lru_cache/tests/integration_tests.rs
+++ lru_cache/tests/integration_tests.rs
@@ -57,4 +57,47 @@ mod tests {
         assert!(cache.is_empty());
         assert_eq!(cache.get(&"B"), None);
     }
+
+    #[test]
+    fn test_lru_cache() {
+        let mut cache = MemoryCache::new(3); // Taille de 3
+        cache.put("A", String::from("value_a"));
+        cache.put("B", String::from("value_b"));
+        cache.put("C", String::from("value_c"));
+        cache.put("D", String::from("value_d"));
+        // Premier élément moins récemment utilisé et dernier le plus récent
+        // Cache == [B, C, D]
+
+        let my_value = cache.get(&"A");
+        assert_eq!(my_value, None);
+        let my_value = cache.get(&"D");
+        assert_eq!(my_value, Some(&String::from("value_d")));
+        // Cache == [B, C, D]
+
+        let my_value = cache.get(&"B");
+        assert_eq!(my_value, Some(&String::from("value_b")));
+        // Cache == [C, D, B]
+
+        let my_value = cache.get(&"C");
+        assert_eq!(my_value, Some(&String::from("value_c")));
+        // Cache == [D, B, C]
+
+        let my_value = cache.get(&"X");
+        assert_eq!(my_value, None);
+        // Cache == [D, B, C]
+
+        cache.put("A", String::from("value_a"));
+        // Cache == [B, C, A]
+
+        cache.put("X", String::from("value_x"));
+        // Cache == [C, A, X]
+
+        let my_value = cache.get(&"B");
+        assert_eq!(my_value, None);
+        // Cache == [C, A, X]
+
+        let my_value = cache.get(&"D");
+        // Cache == [C, A, X]
+        assert_eq!(my_value, None);
+    }
 }
